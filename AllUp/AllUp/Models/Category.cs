1260c61d549624beafc1fe9c28438c968f29c6ab
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-10-04 20:54:57 +0400

    add Category Model

---
+++ AllUp/AllUp/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace AllUp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public bool IsDeactive { get; set; }
+        public bool IsMain{ get; set; }
+
+
+    }
+}
