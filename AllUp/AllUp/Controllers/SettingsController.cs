5b3f61a4752fe0cf4db96f283e9d4e7b406c8298
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-10-06 15:42:14 +0400

    I added the map script to the contact view

--- AllUp/AllUp/Controllers/SettingsController.cs
+++ AllUp/AllUp/Controllers/SettingsController.cs
@@ -4,10 +4,21 @@ namespace AllUp.Controllers
 {
     public class SettingsController : Controller
     {
+        public IActionResult Account()
+        {
+            return View();
+        }
         public IActionResult Checkout()
         {
             return View();
         }
-      
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
