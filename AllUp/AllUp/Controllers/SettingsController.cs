610a4767c1b776725e0b9d26fd8edf9770bf8312
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-10-06 12:55:56 +0400

    add controllers and views

---
+++ AllUp/AllUp/Controllers/SettingsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllUp.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+      
+    }
+}
