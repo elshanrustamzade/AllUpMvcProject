d70a3982af97007c7ca4b67c595d2b16e4a38698
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-10-27 13:17:01 +0400

    add Product Images for Create view Products

--- AllUp/AllUp/Areas/Admin/Controllers/ProductsController.cs
+++ AllUp/AllUp/Areas/Admin/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using AllUp.DAL;
+using AllUp.Helpers;
 using AllUp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,9 +10,12 @@ namespace AllUp.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private readonly AppDbContext _db;
-        public ProductsController(AppDbContext db)
+        private readonly IWebHostEnvironment _env;
+        public ProductsController(AppDbContext db, IWebHostEnvironment env)
         {
             _db = db;
+            _env = env;
+
         }
         public async Task<IActionResult> Index()
         {
@@ -25,6 +29,60 @@ namespace AllUp.Areas.Admin.Controllers
             ViewBag.MainCategories = await _db.Categories.Where(x => x.IsMain).ToListAsync();
             ViewBag.ChildCategories = (await _db.Categories.Include(x => x.Children).FirstOrDefaultAsync())?.Children;
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product, int brandId, int mainCatId, int childCatId, int[] tagsId)
+        {
+
+            #region Get
+            ViewBag.Brands = await _db.Brands.ToListAsync();
+            ViewBag.Tags = await _db.Tags.ToListAsync();
+            ViewBag.MainCategories = await _db.Categories.Where(x => x.IsMain).ToListAsync();
+            ViewBag.ChildCategories = (await _db.Categories.Include(x => x.Children).FirstOrDefaultAsync())?.Children;
+            #endregion
+
+            #region IsExist
+            bool isExist = await _db.Products.AnyAsync(x => x.Name == product.Name);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "This product is already exist!");
+                return View();
+            }
+            #endregion
+
+            #region Photos Save
+            List<ProductImage> productImages = new();
+            foreach (IFormFile photo in product.Photos)
+            {
+                if (photo == null)
+                {
+                    ModelState.AddModelError("Photo", "please select photo");
+                    return View();
+                }
+                if (!photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "please select image type");
+                    return View();
+                }
+                if (photo.IsOrder1Mb())
+                {
+                    ModelState.AddModelError("Photo", "Max 1Mb");
+                    return View();
+                }
+                string folder = Path.Combine(_env.WebRootPath, "assets", "images", "product");
+                ProductImage productImage = new()
+                {
+                    Image = await photo.SaveFileAsync(folder)
+                };
+                productImages.Add(productImage);
+            }
+            product.ProductImages = productImages;
+            #endregion
+
+            await _db.Products.AddAsync(product);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
         public async Task<IActionResult> LoadChildCategories(int mainCategoryId)
         {
